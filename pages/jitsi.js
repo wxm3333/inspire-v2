eaa031e245fa157199ba4bd6f77ddfe1482f14bb
wxm3333 <xiaomin.wang36@gmail.com>
2020-07-31 23:26:54 +0000

    added end meeting button in Jitsi tab

--- pages/jitsi.js
+++ pages/jitsi.js
@@ -12,63 +12,68 @@ function JitsiMeeting () {
   const [password, setPassword] = useState('');
   const [onCall, setOnCall] = useState(false);
 
-	return onCall
-    ? (
-      <Jitsi
-        roomName={roomName}
-        displayName={displayName}
-        password={password}
-        frameStyle={{ display:'block', width:'100%', height:'100%' }}
-        containerStyle={{ width: '1200px', height: '600px' }}
-        onAPILoad={JitsiMeetAPI => console.log('Good Morning everyone!')}
-      />)
-    : (
+	return (
+    <>
+      <h3>Upcoming Meeting</h3>
+      <p>Room name: inspire-jitsi-test; Password: 123</p>
+      <input
+        className="form-input"
+        type='text'
+        placeholder='Room name'
+        value={roomName}
+        onChange={e => setRoomName(e.target.value)}
+      />
+      <input
+        className="form-input"
+        type='text'
+        placeholder='Your name'
+        value={displayName}
+        onChange={e => setDisplayName(e.target.value)}
+      />
+      <input 
+        className="form-input"
+        type='text'
+        placeholder='Password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        onClick={() => setOnCall(true)}
+      >
+        Join Meeting
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => setOnCall(false)}
+      >
+        End Meeting
+      </Button>
 
-      <>
-        <h3>Upcoming Meeting</h3>
-        <p>Room name: inspire-jitsi-test; Password: 123</p>
-        <input
-          className="form-input"
-          type='text'
-          placeholder='Room name'
-          value={roomName}
-          onChange={e => setRoomName(e.target.value)}
-        />
-        <input
-          className="form-input"
-          type='text'
-          placeholder='Your name'
-          value={displayName}
-          onChange={e => setDisplayName(e.target.value)}
-        />
-        <input 
-          className="form-input"
-          type='text'
-          placeholder='Password'
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          onClick={() => setOnCall(true)}
-        >
-          Join Meeting
-        </Button>
+      {onCall &&
+        <Jitsi
+          roomName={roomName}
+          displayName={displayName}
+          password={password}
+          frameStyle={{ display:'block', width:'100%', height:'100%' }}
+          containerStyle={{ width: '1200px', height: '600px' }}
+          onAPILoad={JitsiMeetAPI => console.log('Good Morning everyone!')}
+        />}
 
-				<style jsx>{`
-					.form-input {
-						border: 1px solid #BABACC;
-						height: 32px;
-						border-radius: 10px;
-						padding: 0;
-						box-sizing: border-box;
-						font-size: 13px;
-						color: #232323;
-						padding: 0 12px;
-						width: 200px;
-            margin-right: 10px;
-					}
-	      `}</style>
+      <style jsx>{`
+        .form-input {
+          border: 1px solid #BABACC;
+          height: 32px;
+          border-radius: 10px;
+          padding: 0;
+          box-sizing: border-box;
+          font-size: 13px;
+          color: #232323;
+          padding: 0 12px;
+          width: 200px;
+          margin-right: 10px;
+        }
+      `}</style>
       </>
     )
 }
