68296301297e3e876d5ef0de6bb711a5a38f176e
Belen C Saldias Fuentes <bcsaldias@uc.cl>
2020-08-04 17:41:00 -0400

    Fixed window doesnt exist through global ZoomMtg import

--- pages/zoom.js
+++ pages/zoom.js
@@ -5,10 +5,11 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { ZoomMtg } from '@zoomus/websdk';
 
 import NavBar from '../components/navbar'
 import styles from './zoom.module.css'
+
+var ZoomMtg = global.ZoomMtg;
 
 function ZoomComponentFunction() {
 	const [meetingLaunched, setMeetingLaunched] = useState(false);
